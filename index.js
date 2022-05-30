1395073bda5a252633985a72dcf5b72ed3108b7c
Arya V L <aryalekshmi9813@gmail.com>
2022-05-30 17:26:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function del() { 
+    let screenValue=document.getElementById('screen').value;
+    document.getElementById('screen').value= screenValue.substr(0,screenValue.length -1)
+ }
+
+ var two=document.getElementById('two');
+		two.addEventListener('click',function(){
+			var body=document.querySelector('body');
+			var toggle=document.getElementById('circle');
+			body.classList.add('active1');
+			body.classList.remove('active2');
+			toggle.style.left='36%';
+		})
+		var one=document.getElementById('one');
+		one.addEventListener('click',function(){
+			var body=document.querySelector('body');
+			var toggle=document.getElementById('circle');
+			body.classList.remove('active1');
+			body.classList.remove('active2');
+			toggle.style.left='0';
+		})
+		var three=document.getElementById('three');
+		three.addEventListener('click',function(){
+			var body=document.querySelector('body');
+			var toggle=document.getElementById('circle');
+			body.classList.add('active2');
+			body.classList.remove('active1');
+			toggle.style.left='65%';
+		})
